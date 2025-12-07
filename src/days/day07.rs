317127cc4c049970b7cdffabf1fe452ff27a86bb
beeb <703631+beeb@users.noreply.github.com>
2025-12-07 10:20:45 +0100

    feat(day07): part 2

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 use winnow::{
     Parser as _, Result,
@@ -10,6 +10,7 @@ use winnow::{
 use crate::days::Day;
 
 const LINES: i32 = 142;
+const COLUMNS: i32 = 141;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Point {
@@ -28,16 +29,70 @@ impl Point {
 
 #[derive(Debug, Clone)]
 pub struct Manifold {
+    /// Start point
     start: Point,
+    /// Positions of all splitters
     splitters: BTreeSet<Point>,
-    beams: BTreeSet<Point>,
+    /// For each beam position, how many paths go through here
+    beams: BTreeMap<Point, usize>,
 }
 
 impl Manifold {
-    fn receives_beam(&self, splitter: Point) -> bool {
-        self.beams.contains(&splitter.above())
+    /// Return whether a beam enter the splitter, and how many paths go through that position
+    fn receives_beam(&self, splitter: Point) -> Option<usize> {
+        self.beams.get(&splitter.above()).copied()
+    }
+
+    fn project_beams(&mut self) -> usize {
+        self.beams.insert(
+            Point {
+                y: 1,
+                x: self.start.x,
+            },
+            1, // 1 path goes through the initial beam
+        );
+        let mut count_splits = 0;
+        for y in 2..LINES {
+            for x in 0..COLUMNS {
+                let p = Point { y, x };
+                if self.splitters.contains(&p)
+                    && let Some(paths) = self.receives_beam(p)
+                {
+                    count_splits += 1;
+                    let left = *self
+                        .beams
+                        .entry(Point { y: p.y, x: p.x - 1 })
+                        .and_modify(|p| *p += paths)
+                        .or_insert(paths);
+                    let right = *self
+                        .beams
+                        .entry(Point { y: p.y, x: p.x + 1 })
+                        .and_modify(|p| *p += paths)
+                        .or_insert(paths);
+                    self.beams.insert(
+                        Point {
+                            y: p.y + 1,
+                            x: p.x - 1,
+                        },
+                        left,
+                    );
+                    self.beams.insert(
+                        Point {
+                            y: p.y + 1,
+                            x: p.x + 1,
+                        },
+                        right,
+                    );
+                } else if let Some(paths) = self.beams.get(&p) {
+                    self.beams.insert(Point { y: p.y + 1, x: p.x }, *paths);
+                }
+            }
+        }
+        count_splits
     }
 }
+
+impl Manifold {}
 
 pub struct Day07;
 
@@ -77,7 +132,7 @@ impl Day for Day07 {
                 x: start_x as i32,
             },
             splitters,
-            beams: BTreeSet::new(),
+            beams: BTreeMap::new(),
         })
     }
 
@@ -85,53 +140,23 @@ impl Day for Day07 {
 
     fn part_1(input: &Self::Input) -> Self::Output1 {
         let mut manifold = input.clone();
-        manifold.beams.insert(Point {
-            y: 1,
-            x: manifold.start.x,
-        });
-        let mut count_splits = 0;
-        for y in 2..LINES {
-            for x in 0..LINES {
-                let p = Point { y, x };
-                if manifold.splitters.contains(&p) && manifold.receives_beam(p) {
-                    count_splits += 1;
-                    manifold.beams.insert(Point { y: p.y, x: p.x - 1 });
-                    manifold.beams.insert(Point { y: p.y, x: p.x + 1 });
-                    manifold.beams.insert(Point {
-                        y: p.y + 1,
-                        x: p.x - 1,
-                    });
-                    manifold.beams.insert(Point {
-                        y: p.y + 1,
-                        x: p.x + 1,
-                    });
-                } else if manifold.beams.contains(&p) {
-                    manifold.beams.insert(Point { y: p.y + 1, x: p.x });
-                }
-            }
-        }
-        // for y in 0..LINES {
-        //     for x in 0..LINES {
-        //         let p = Point { y, x };
-        //         if p == manifold.start {
-        //             print!("S");
-        //         } else if manifold.splitters.contains(&p) {
-        //             print!("^");
-        //         } else if manifold.beams.contains(&p) {
-        //             print!("|");
-        //         } else {
-        //             print!(".");
-        //         }
-        //     }
-        //     println!();
-        // }
-        count_splits
+        manifold.project_beams()
     }
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let mut manifold = input.clone();
+        manifold.project_beams();
+        let last_line = Point { y: LINES - 1, x: 0 }..Point {
+            y: LINES - 1,
+            x: COLUMNS,
+        };
+        manifold
+            .beams
+            .range(last_line)
+            .map(|(_, paths)| *paths)
+            .sum()
     }
 }
 
@@ -166,6 +191,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let parsed = Day07::parser(&mut INPUT).unwrap();
-        assert_eq!(Day07::part_2(&parsed), 14);
+        assert_eq!(Day07::part_2(&parsed), 40);
     }
 }
